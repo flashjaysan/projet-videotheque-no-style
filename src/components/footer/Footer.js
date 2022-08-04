48409414b45ee9d3f564b21f03fd17592b1ab3d3
flashjaysan <jaysan@wanadoo.fr>
2022-08-04 17:23:13 +0200

    Update Footer.js

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,7 +1,13 @@
 const Footer = () => {
   return (
     <div className="p-centered">
-      <footer>© Jérôme BARBIER, Nicolas FERCHAUD, Esteban MARTEL</footer>
+      <footer>
+        Par Jérôme BARBIER, Nicolas FERCHAUD, Esteban MARTEL. Le code source est
+        sous licence
+        <a href="http://opensource.org/licenses/mit-license.php">MIT</a>. Cette
+        application utilise l'API TMDB API mais n'est pas soutenu ni certifié
+        par TMDB.
+      </footer>
     </div>
   );
 };
