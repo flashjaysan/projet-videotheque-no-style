0a18a34395677167e6debc3a60be2805722db57c
flashjaysan <jaysan@wanadoo.fr>
2022-08-04 18:51:32 +0200

    centrage du footer, fond similaire à la navbar

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,14 +1,20 @@
 const Footer = () => {
   return (
-    <div className="p-centered">
-      <footer>
+    <footer className="bg-primary ">
+      <p className="text-center">
         Par Jérôme BARBIER, Nicolas FERCHAUD, Esteban MARTEL. Le code source est
         sous licence
-        <a href="http://opensource.org/licenses/mit-license.php">MIT</a>. Cette
-        application utilise l'API TMDB API mais n'est pas soutenu ni certifié
-        par TMDB.
-      </footer>
-    </div>
+        <a
+          href="http://opensource.org/licenses/mit-license.php"
+          class="text-light"
+        >
+          {" "}
+          MIT
+        </a>
+        . Cette application utilise l'API TMDB mais n'est pas soutenu ni
+        certifié par TMDB.
+      </p>
+    </footer>
   );
 };
 
